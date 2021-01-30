d1ed0d6caf20266248db97361c5317e3c5dd0793
Alex Alexis <aalexis8@gmail.com>
2021-01-30 09:40:13 -0500

    promises instead of callback before fetch api

--- app.js
+++ app.js
@@ -25,11 +25,44 @@ const posts = [
 
 // Async with callback
 // We can pass in getPosts which will be called before the function ends
-function createPost(post, callback) {
-  setTimeout(function () {
-    posts.push(post);
-    callback();
-  }, 2000);
+// function createPost(post, callback) {
+//   setTimeout(function () {
+//     posts.push(post);
+//     callback();
+//   }, 2000);
+// }
+
+// function getPosts() {
+//   setTimeout(function () {
+//     let output = "";
+//     posts.forEach(function (post) {
+//       output += `<li>${post.title}</li>`;
+//     });
+//     document.body.innerHTML = output;
+//   }, 1000);
+// }
+
+// createPost({ title: "Post Three", body: "This our third post" }, getPosts);
+// A function that can be passed in and then called within the function they were passed into
+
+//an alternative to callbacks Or an alternative way of handling asynchronous operations
+// Promises ---
+// While handling asynchronous operations they can promise to do something else. Below we will transform the call way.. into a promise
+
+// remove the callback function
+function createPost(post) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      posts.push(post);
+      const error = false;
+
+      if (!error) {
+        resolve();
+      } else {
+        reject("Error: Something went awry!!");
+      }
+    }, 2000);
+  });
 }
 
 function getPosts() {
@@ -42,5 +75,8 @@ function getPosts() {
   }, 1000);
 }
 
-createPost({ title: "Post Three", body: "This our third post" }, getPosts);
-// A function that can be passed in and then called within the function they were passed into
+createPost({ title: "Post Three", body: "This our third post" })
+  .then(getPosts)
+  .catch(function (err) {
+    console.log(err);
+  });
